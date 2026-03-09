74b600a099cfba4974cf90842a480e731c296a8a
ld0995145 <lukeduggins46@gmail.com>
2026-03-09 09:43:31 -0500

    Still need to fix name and make it required

---
+++ app/src/main/java/com/example/pokemoncafeapp/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.pokemoncafeapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnSend = findViewById<Button>(R.id.btnSend)
+        val itemRadio = findViewById<RadioGroup>(R.id.radioGroup)
+        val editName = findViewById<EditText>(R.id.editName)
+        val diningCheck = findViewById<CheckBox>(R.id.diningCheck)
+
+        btnSend.setOnClickListener {
+            val customerName = editName.text
+
+            if (TextUtils.isEmpty(customerName)){
+                val toast = Toast.makeText(this, "Please insert your name", Toast.LENGTH_SHORT)
+                toast.show()
+
+                // Throw error here and prevent moving to next activity
+
+            }
+
+            val isDining = diningCheck.isChecked
+
+            val selectedId = itemRadio.checkedRadioButtonId
+            val selectedItem = findViewById<RadioButton>(selectedId)
+            val itemName = selectedItem.text.toString()
+
+            var receiptTotal = 0
+            if (selectedItem.text == "$3 Koffing Koffee"){
+                receiptTotal = 3
+            } else if (selectedItem.text == "$5 Alcreamie Cone"){
+                receiptTotal = 5
+            } else if (selectedItem.text == "$10 Palossand-wiches"){
+                receiptTotal = 10
+            } else if (selectedItem.text == "$4 Fidough-nuts"){
+                receiptTotal = 4
+            }
+
+            if (isDining == false) {
+                receiptTotal += 5
+            }
+
+            Log.d("EXTRAS_SENT", "Name: $customerName")
+            Log.d("EXTRAS_SENT", "Selected Item: $itemName")
+            Log.d("EXTRAS_SENT", "Dining in: $isDining")
+            Log.d("EXTRAS_SENT", "Receipt Total: $receiptTotal")
+
+            val intent = Intent(this@MainActivity, ReceiptActivity::class.java)
+            intent.putExtra("customerName", customerName)
+            intent.putExtra("item", itemName)
+            intent.putExtra("isDining", isDining)
+            intent.putExtra("receiptTotal", receiptTotal)
+
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/pokemoncafeapp/ReceiptActivity.kt
@@ -0,0 +1,75 @@
+package com.example.pokemoncafeapp
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+private const val TAG = "LifecyleShow"
+class ReceiptActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_receipt)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Capture the text boxes to change
+        val textName = findViewById<TextView>(R.id.textName)
+        val textItem = findViewById<TextView>(R.id.textItem)
+        val textDining = findViewById<TextView>(R.id.textDining)
+        val textPrice = findViewById<TextView>(R.id.textPrice)
+
+        // Read the extras
+        val customerName = intent.getStringExtra("customerName")
+        val item = intent.getStringExtra("item")
+        val dining = intent.getBooleanExtra("isDining", true)
+        val price = intent.getIntExtra("receiptTotal", 0)
+
+        // If block to better format about the dining
+        var diningText = ""
+        if (dining){
+            diningText = "Dining in, no charge added"
+        } else {
+            diningText = "Dining out, $5 fee added"
+        }
+
+        // Change all the texts according to what was passed through the intent
+        textName.text = "Order name: $customerName"
+        textItem.text = "You ordered: $item"
+        textDining.text = diningText
+        textPrice.text = "Total: $$price"
+    }
+
+    // Section for Lifecycle logs
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart → happens when Activity becomes visible")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume → happens when Activity is in the foreground and interactive")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause → happens when you press Home or open another Activity")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop → happens after Home or when Activity is fully hidden")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart → happens when returning from Recents after Home")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy → happens when Back is pressed or Activity is finishing (not guaranteed)")
+    }
+}
